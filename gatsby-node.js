e982d62fe750dae32cd1dfe7a0155c071d6daefd
imchanyo <chanyoung3320@gmail.com>
2021-06-23 08:45:53 +0900

    feature/business 반응형수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
 /**
  * Implement Gatsby's Node APIs in this file.
  *
- * See: https://www.gatsbyjs.com/docs/node-apis/
+ * See: <https://www.gatsbyjs.com/docs/node-apis/>
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path');
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        //옵션을 설정하는 부분입니다.
+        //components로 시작하는 경로는 모두 src 폴더 내의 components 폴더로 매핑해주어 절대 경로를 사용할 수 있도록 해주는 부분입니다.
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+  });
+};
