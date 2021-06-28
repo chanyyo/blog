f7929f83970b475c0e6b36c0ac71a79a95c25013
kn00371 <kn00371@partner.sk.com>
2021-06-28 18:56:12 +0900

    포스트 아이템 링크 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,14 +87,17 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
 
 export default IndexPage;
 
-export const queryPostList = graphql`
-  query queryPostList {
+export const getPostList = graphql`
+  query getPostList {
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
     ) {
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
