0e5a0cc309bfdd9f57728761c3e15b5ec8be768b
imchanyo <chanyoung3320@gmail.com>
2021-10-26 17:00:03 +0900

    eeqweq

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: FunctionComponent = function () {
   return (
     <FooterWrapper>
       Thank You for Visiting My Blog, Have a Good Day 😆
-      <br />© 2021 Developer Chan, Powered By <Gatsby111></Gatsby111>.
+      <br />© 2021 Developer Chan, Powered By Gatsby111.
     </FooterWrapper>
   );
 };
